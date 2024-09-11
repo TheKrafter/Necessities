4f39cc2fccd0acc6d44fa232656310090cb85e73
TheKrafter <75903664+TheKrafter@users.noreply.github.com>
2024-09-10 19:38:52 -0500

    Initial commit

---
+++ src/main/kotlin/xyz/krafterdev/necessities/Necessities.kt
@@ -0,0 +1,14 @@
+package xyz.krafterdev.necessities
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class Necessities : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
